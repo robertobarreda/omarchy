774b53479e98fd7a2e96986b06fbbb6f20bd766b
Ryan Hughes <ryan@heyoodle.com>
2025-08-31 00:07:47 +0300

    Don't trigger rebuild. We already rebuild later.

--- install/login/plymouth.sh
+++ install/login/plymouth.sh
@@ -8,7 +8,7 @@
 
 if [ "$(plymouth-set-default-theme)" != "omarchy" ]; then
   sudo cp -r "$HOME/.local/share/omarchy/default/plymouth" /usr/share/plymouth/themes/omarchy/
-  sudo plymouth-set-default-theme -R omarchy
+  sudo plymouth-set-default-theme omarchy
 fi
 
 # ==============================================================================
