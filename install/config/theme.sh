4bbe9649c8fb17d1d1c8d993d08faf4a7ae68512
Ryan Hughes <ryan@heyoodle.com>
2025-08-31 21:38:17 +0300

    Revert "Test gsettings"
    This reverts commit 49c27d319407f6c95fcbb4c5a2646e54b50c9ab4.

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
-
 # Set links for Nautilius action icons
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
