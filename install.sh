0bfe184f28531ea33c106c90fc16953617928258
Ryan Hughes <ryan@heyoodle.com>
2025-08-31 17:04:04 +0300

    Fix rebase doubling up

--- install.sh
+++ install.sh
@@ -59,5 +59,3 @@ source $OMARCHY_INSTALL/login/alt-bootloaders.sh
 # Finishing
 source $OMARCHY_INSTALL/post-install.sh
 source $OMARCHY_INSTALL/reboot.sh
-source $OMARCHY_INSTALL/post-install.sh
-source $OMARCHY_INSTALL/reboot.sh
