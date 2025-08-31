af4bf789a501cdc096009cb5720e933551e56580
Ryan Hughes <ryan@heyoodle.com>
2025-08-31 19:31:38 +0300

    Add run wrapper function for feedback

--- install.sh
+++ install.sh
@@ -7,55 +7,59 @@ OMARCHY_PATH="$HOME/.local/share/omarchy"
 OMARCHY_INSTALL="$OMARCHY_PATH/install"
 export PATH="$OMARCHY_PATH/bin:$PATH"
 
+run() {
+  echo "Executing $(basename "$1")..."
+  source "$1"
+  echo "Finished executing $(basename "$1")"
+}
+
 # Preparation
-source $OMARCHY_INSTALL/preflight/show-env.sh
-source $OMARCHY_INSTALL/preflight/trap-errors.sh
-source $OMARCHY_INSTALL/preflight/guard.sh
-source $OMARCHY_INSTALL/preflight/chroot.sh
-source $OMARCHY_INSTALL/preflight/pacman.sh
-source $OMARCHY_INSTALL/preflight/migrations.sh
-source $OMARCHY_INSTALL/preflight/first-run-mode.sh
+run $OMARCHY_INSTALL/preflight/show-env.sh
+run $OMARCHY_INSTALL/preflight/trap-errors.sh
+run $OMARCHY_INSTALL/preflight/guard.sh
+run $OMARCHY_INSTALL/preflight/chroot.sh
+run $OMARCHY_INSTALL/preflight/pacman.sh
+run $OMARCHY_INSTALL/preflight/migrations.sh
+run $OMARCHY_INSTALL/preflight/first-run-mode.sh
 
 # Packaging
-source $OMARCHY_INSTALL/packages.sh
-source $OMARCHY_INSTALL/packaging/fonts.sh
-source $OMARCHY_INSTALL/packaging/lazyvim.sh
-source $OMARCHY_INSTALL/packaging/webapps.sh
-source $OMARCHY_INSTALL/packaging/tuis.sh
+run $OMARCHY_INSTALL/packages.sh
+run $OMARCHY_INSTALL/packaging/fonts.sh
+run $OMARCHY_INSTALL/packaging/lazyvim.sh
+run $OMARCHY_INSTALL/packaging/webapps.sh
+run $OMARCHY_INSTALL/packaging/tuis.sh
 
 # Configuration
-source $OMARCHY_INSTALL/config/config.sh
-source $OMARCHY_INSTALL/config/theme.sh
-source $OMARCHY_INSTALL/config/branding.sh
-source $OMARCHY_INSTALL/config/git.sh
-source $OMARCHY_INSTALL/config/gpg.sh
-source $OMARCHY_INSTALL/config/timezones.sh
-source $OMARCHY_INSTALL/config/increase-sudo-tries.sh
-source $OMARCHY_INSTALL/config/increase-lockout-limit.sh
-source $OMARCHY_INSTALL/config/ssh-flakiness.sh
-source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-source $OMARCHY_INSTALL/config/xcompose.sh
-source $OMARCHY_INSTALL/config/mise-ruby.sh
-source $OMARCHY_INSTALL/config/docker.sh
-source $OMARCHY_INSTALL/config/mimetypes.sh
-source $OMARCHY_INSTALL/config/localdb.sh
-source $OMARCHY_INSTALL/config/sudoless-asdcontrol.sh
-source $OMARCHY_INSTALL/config/hardware/network.sh
-source $OMARCHY_INSTALL/config/hardware/set-wireless-regdom.sh
-source $OMARCHY_INSTALL/config/hardware/fix-fkeys.sh
-source $OMARCHY_INSTALL/config/hardware/bluetooth.sh
-source $OMARCHY_INSTALL/config/hardware/printer.sh
-source $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
-source $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
-source $OMARCHY_INSTALL/config/hardware/nvidia.sh
-source $OMARCHY_INSTALL/config/hardware/intel.sh
-source $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
+run $OMARCHY_INSTALL/config/config.sh
+run $OMARCHY_INSTALL/config/theme.sh
+run $OMARCHY_INSTALL/config/branding.sh
+run $OMARCHY_INSTALL/config/git.sh
+run $OMARCHY_INSTALL/config/gpg.sh
+run $OMARCHY_INSTALL/config/timezones.sh
+run $OMARCHY_INSTALL/config/increase-sudo-tries.sh
+run $OMARCHY_INSTALL/config/increase-lockout-limit.sh
+run $OMARCHY_INSTALL/config/ssh-flakiness.sh
+run $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
+run $OMARCHY_INSTALL/config/xcompose.sh
+run $OMARCHY_INSTALL/config/mise-ruby.sh
+run $OMARCHY_INSTALL/config/docker.sh
+run $OMARCHY_INSTALL/config/mimetypes.sh
+run $OMARCHY_INSTALL/config/localdb.sh
+run $OMARCHY_INSTALL/config/sudoless-asdcontrol.sh
+run $OMARCHY_INSTALL/config/hardware/network.sh
+run $OMARCHY_INSTALL/config/hardware/fix-fkeys.sh
+run $OMARCHY_INSTALL/config/hardware/bluetooth.sh
+run $OMARCHY_INSTALL/config/hardware/printer.sh
+run $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
+run $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
+run $OMARCHY_INSTALL/config/hardware/nvidia.sh
+run $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
 
 # Login
-source $OMARCHY_INSTALL/login/plymouth.sh
-source $OMARCHY_INSTALL/login/limine-snapper.sh
-source $OMARCHY_INSTALL/login/alt-bootloaders.sh
+run $OMARCHY_INSTALL/login/plymouth.sh
+run $OMARCHY_INSTALL/login/limine-snapper.sh
+run $OMARCHY_INSTALL/login/alt-bootloaders.sh
 
 # Finishing
-source $OMARCHY_INSTALL/post-install.sh
-source $OMARCHY_INSTALL/reboot.sh
+run $OMARCHY_INSTALL/post-install.sh
+run $OMARCHY_INSTALL/reboot.sh
