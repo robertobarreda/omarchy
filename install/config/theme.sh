2225bd9ebd851dc6fd2cb5edad0755f061ec1c84
Ryan Hughes <ryan@heyoodle.com>
2025-08-31 20:24:44 +0300

    Move gnome updates to first-run

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
-
 # Set links for Nautilius action icons
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
-sudo gtk-update-icon-cache /usr/share/icons/Yaru
 
 # Setup theme links
 mkdir -p ~/.config/omarchy/themes
