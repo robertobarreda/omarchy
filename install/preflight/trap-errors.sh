dfcb0f5943351380ac25bfaca75066c6b4a333fb
Ryan Hughes <ryan@heyoodle.com>
2025-09-03 00:56:55 +0200

    Update trap

--- install/preflight/trap-errors.sh
+++ install/preflight/trap-errors.sh
@@ -2,17 +2,16 @@
 
 # Give people a chance to retry running the installation
 catch_errors() {
-  echo -e "\n\e[31mOmarchy installation failed!\e[0m"
-  echo
-  echo "This command halted with exit code $?:"
-  echo "$BASH_COMMAND"
-  echo
-
   # Show the last lines of the log to help debug
   if [ -n "${LOG_FILE:-}" ] && [ -f "$LOG_FILE" ]; then
     tail -50 "$LOG_FILE"
   fi
 
+  echo -e "\n\e[31mOmarchy installation failed!\e[0m"
+  echo
+  echo "This command halted with exit code $?:"
+  echo "$BASH_COMMAND"
+  echo
   echo
   echo "Get help from the community via QR code or at https://discord.gg/tXFUdasqhY"
   echo "                                 "
