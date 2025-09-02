68e61b3e1f7360cc0db57a2cb9a8b2df917228eb
Ryan Hughes <ryan@heyoodle.com>
2025-09-03 00:14:21 +0200

    Center up reboot notice

--- install/reboot.sh
+++ install/reboot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 clear
-tte -i ~/.local/share/omarchy/logo.txt --frame-rate 920 laseretch
+tte -i ~/.local/share/omarchy/logo.txt --canvas-width 0 --anchor-text c --frame-rate 920 laseretch
 echo
-echo "You're done! So we're ready to reboot now..." | tte --frame-rate 640 wipe
+echo "You're done! So we're ready to reboot now..." | tte --canvas-width 0 --anchor-text c --frame-rate 640 wipe
 
 if sudo test -f /etc/sudoers.d/99-omarchy-installer; then
   sudo rm -f /etc/sudoers.d/99-omarchy-installer &>/dev/null
-  echo -e "\nRemember to remove USB installer!\n\n"
+  echo
+  echo "Remember to remove USB installer!" | tte --canvas-width 0 --anchor-text c --frame-rate 640 wipe
 fi
 
 sleep 5
