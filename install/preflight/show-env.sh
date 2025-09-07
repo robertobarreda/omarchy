c0ec74fce20a78dee9ac3ad67911303904d9dd18
Ryan Hughes <ryan@heyoodle.com>
2025-09-07 18:00:21 +0200

    Use gum log to output cleaner

--- install/preflight/show-env.sh
+++ install/preflight/show-env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "Installation ENV:"
-env | grep -E "^(OMARCHY_CHROOT_INSTALL|OMARCHY_OFFLINE_INSTALL|OMARCHY_USER_NAME|OMARCHY_USER_EMAIL|USER|HOME|OMARCHY_REPO|OMARCHY_REF)="
+# Show installation environment variables
+gum log --level info "Installation Environment:"
+
+env | grep -E "^(OMARCHY_CHROOT_INSTALL|OMARCHY_OFFLINE_INSTALL|OMARCHY_USER_NAME|OMARCHY_USER_EMAIL|USER|HOME|OMARCHY_REPO|OMARCHY_REF)=" | sort | while IFS= read -r var; do
+  gum log --level info "  $var"
+done
