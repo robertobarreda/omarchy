5557ed0837c18032e3932fb532b57c1957d7ec51
Ryan Hughes <1630358+ryanrhughes@users.noreply.github.com>
2025-09-11 18:17:16 -0400

    Add OMARCHY_PATH

--- install/preflight/show-env.sh
+++ install/preflight/show-env.sh
@@ -3,6 +3,6 @@
 # Show installation environment variables
 gum log --level info "Installation Environment:"
 
-env | grep -E "^(OMARCHY_CHROOT_INSTALL|OMARCHY_OFFLINE_INSTALL|OMARCHY_USER_NAME|OMARCHY_USER_EMAIL|USER|HOME|OMARCHY_REPO|OMARCHY_REF)=" | sort | while IFS= read -r var; do
+env | grep -E "^(OMARCHY_CHROOT_INSTALL|OMARCHY_OFFLINE_INSTALL|OMARCHY_USER_NAME|OMARCHY_USER_EMAIL|USER|HOME|OMARCHY_REPO|OMARCHY_REF|OMARCHY_PATH)=" | sort | while IFS= read -r var; do
   gum log --level info "  $var"
 done
