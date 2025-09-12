e9b438ebc0acf61b05b081cfecb0da5810f40bb8
Ryan Hughes <1630358+ryanrhughes@users.noreply.github.com>
2025-09-12 15:11:15 -0400

    Elim verbosity

--- install/config/hardware/fix-apple-bcm4360.sh
+++ install/config/hardware/fix-apple-bcm4360.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Look for BCM4360 802.11ac chipset on Apple
-echo "Checking for Apple BCM4360 wireless chipset..."
 if lspci -nnv | grep -A2 "14e4:43a0" | grep -q "106b:"; then
-  echo "Apple BCM4360 detected, installing broadcom-wl driver..."
+  echo "Apple BCM4360 detected"
   sudo pacman -S --noconfirm --needed broadcom-wl dkms linux-headers
-else
-  echo "Apple BCM4360 not detected, skipping driver installation"
 fi
