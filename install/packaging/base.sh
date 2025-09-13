18a785b6cf392cebc5a64fd7278b447b41402bc4
David Heinemeier Hansson <david@hey.com>
2025-09-13 16:40:47 +0200

    No need for bashing

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
+# Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
-
 sudo pacman -S --noconfirm --needed "${packages[@]}"
