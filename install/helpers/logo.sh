9eac02c0983a6a924cadc12b7d49d2d69d14e544
David Heinemeier Hansson <david@hey.com>
2025-09-13 16:17:30 +0200

    Style

--- install/helpers/logo.sh
+++ install/helpers/logo.sh
@@ -1,6 +1,4 @@
 clear_logo() {
-  # Clear screen and move cursor to top-left
-  printf "\033[H\033[2J"
-
+  printf "\033[H\033[2J" # Clear screen and move cursor to top-left
   gum style --foreground 2 --padding "1 0 0 $PADDING_LEFT" "$(<"$LOGO_PATH")"
 }
