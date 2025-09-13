ffcbfb498f88830c2b2c77983305f6e7f40d0e67
David Heinemeier Hansson <david@hey.com>
2025-09-13 15:30:25 +0200

    CRs

--- install.sh
+++ install.sh
@@ -25,9 +25,7 @@ source $OMARCHY_INSTALL/preflight/guard.sh # Need to be able to prompt
 source $OMARCHY_INSTALL/preflight/start-logs.sh
 
 clear_logo
-
 gum style --foreground 3 --padding "1 0 0 $PADDING_LEFT" "Installing Omarchy..."
-
 start_log_output
 
 # Preparation
