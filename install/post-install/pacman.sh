1d38781a1f30dd78978a4e6dc578ad2bac1618fc
Ryan Hughes <ryan@heyoodle.com>
2025-09-14 13:40:59 -0400

    Use GitHub mirror for now

--- install/post-install/pacman.sh
+++ install/post-install/pacman.sh
@@ -6,7 +6,7 @@ if lspci -nn | grep -q "106b:180[12]"; then
   cat <<EOF | sudo tee -a /etc/pacman.conf >/dev/null
 
 [arch-mact2]
-Server = https://mirror.funami.tech/arch-mact2/os/x86_64
+Server = https://github.com/NoaHimesaka1873/arch-mact2-mirror/releases/download/release
 SigLevel = Never
 EOF
 fi
