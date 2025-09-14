b880a2c2e2f4f79d37139983978ac7eee62b391e
Ryan Hughes <ryan@heyoodle.com>
2025-09-14 00:55:57 -0400

    Trim

--- install/post-install/pacman.sh
+++ install/post-install/pacman.sh
@@ -3,7 +3,6 @@ sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
 sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
 
 if lspci -nn | grep -q "106b:180[12]"; then
-  echo "Adding T2 MacBook repository to pacman.conf..."
   cat <<EOF | sudo tee -a /etc/pacman.conf >/dev/null
 
 [arch-mact2]
