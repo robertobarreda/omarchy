1568a5a4e170d9d5d7014e0a269ecb2370e09efa
Roberto Barreda <roberto.barreda@gmail.com>
2025-09-23 12:01:22 +0200

    simplify password manager install and setup

--- migrations/1758207683.sh
+++
@@ -1,6 +0,0 @@
-echo "Copy configs for keepassxc as alternative password manager options"
-
-if [[ ! -f ~/.config/keepassxc/keepassxc.ini ]]; then
-  mkdir -p ~/.config/keepassxc/
-  cp -Rpf $OMARCHY_PATH/config/keepassxc/keepassxc.ini ~/.config/keepassxc/keepassxc.ini
-fi
